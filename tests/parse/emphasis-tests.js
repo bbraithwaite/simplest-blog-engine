6e44705f095134aee6adc767396676e877a50af5
Bradley Braithwaite <hi@bradoncode.com>
2024-03-14 20:44:30 +0100

    Use ~~ for em tags.

--- tests/parse/emphasis-tests.js
+++ tests/parse/emphasis-tests.js
@@ -21,25 +21,25 @@ testThat(
 
 testThat(
   'italic tags are parsed',
-  test.parseMarkUp('--italic tags-- are converted, --more than 2 times--.'),
+  test.parseMarkUp('~~italic tags~~ are converted, ~~more than 2 times~~.'),
   '<p><em>italic tags</em> are converted, <em>more than 2 times</em>.</p>'
 )
 
 testThat(
   'italic tags are parsed',
-  test.parseMarkUp('In this snippet --italics-- are converted, --more than once--.'),
+  test.parseMarkUp('In this snippet ~~italics~~ are converted, ~~more than once~~.'),
   '<p>In this snippet <em>italics</em> are converted, <em>more than once</em>.</p>'
 )
 
 testThat(
   'escaped italic tags are parsed',
-  test.parseMarkUp('In this snippet --ital\\-ics-- are converted, --more than once--.'),
+  test.parseMarkUp('In this snippet ~~ital\\-ics~~ are converted, ~~more than once~~.'),
   '<p>In this snippet <em>ital&#8208;ics</em> are converted, <em>more than once</em>.</p>'
 )
 
 testThat(
   'nested emphasis tags are parsed',
-  test.parseMarkUp('In this snippet --**italics**-- are converted, **--more than once--**.'),
+  test.parseMarkUp('In this snippet ~~**italics**~~ are converted, **~~more than once~~**.'),
   '<p>In this snippet <em><strong>italics</strong></em> are converted, <strong><em>more than once</em></strong>.</p>'
 )
 
